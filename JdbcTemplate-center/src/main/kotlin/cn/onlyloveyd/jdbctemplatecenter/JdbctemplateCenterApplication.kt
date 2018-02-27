e429750c702e35f18f1a0662a44049f717fdfaa2
onlyloveyd@gmail.com <Yd@19911219>
2018-02-27 13:49:11 +0800

    jdbctemplate操作数据库

---
+++ JdbcTemplate-center/src/main/kotlin/cn/onlyloveyd/jdbctemplatecenter/JdbctemplateCenterApplication.kt
@@ -0,0 +1,11 @@
+package cn.onlyloveyd.jdbctemplatecenter
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JdbctemplateCenterApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(JdbctemplateCenterApplication::class.java, *args)
+}
